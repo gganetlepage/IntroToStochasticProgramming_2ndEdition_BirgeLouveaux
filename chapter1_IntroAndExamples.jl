69bf4e3372140083e4872c749a40f74c8e2f033b
guiganlep <g.ganet.lepage@gmail.com>
2020-10-12 12:50:02 +0200

    uncertainty of yield treated

--- chapter1_IntroAndExamples.jl
+++ chapter1_IntroAndExamples.jl
@@ -1,20 +1,11 @@
 #the same notation as in the book is used in order to compare promptly.
 using JuMP, Gurobi
 
-
-#Parameters
-land = 500
-cattle = [200,240]
-sell_mean = [170,150,36,10]
-buy_mean = 1.4 * sell_mean[1:2]
-quota = 6000
-yield_mean = [2.5,3,20]
-plant = [150,230,260]
-
+const GUROBI_ENV = Gurobi.Env() # This constant able not to see more than once "Academic license - for non-commercial use only"
 
 "Add a new model, create the variables with their domain, the objective and the constraints"
 function add_model(land, cattle, sell, buy, yield, quota, plant, print_model = false)
-    model = Model(Gurobi.Optimizer)
+    model = Model(with_optimizer(Gurobi.Optimizer,GUROBI_ENV)) # GUROBI_ENV to hide the gurobi message
     function add_variables(model)
         @variables(model, begin
         x[i=1:3] >= 0, (base_name = "x_$i") # acres devoted to a resource to produce
@@ -41,31 +32,56 @@ function add_model(land, cattle, sell, buy, yield, quota, plant, print_model = f
     x,y,z = add_variables(model)
     add_objective(model, x, y, z, sell, buy, plant)
     add_constraints(model, x, y, z, yield, quota)
+    print_model && println(model)
     model
 end
 
 "Solve the model, return: the objective value, x, w, y)"
-function solve_model(model)
+function solve_model(model, print_result = false)
     optimize!(model)
-    println(termination_status(model))
+    if print_result
+        println(termination_status(model))
+        println("objective_value:", objective_value(model))
+        println(value.(model[:x]), value.(model[:w]), value.(model[:y]))
+    end
     objective_value(model), value.(model[:x]), value.(model[:w]), value.(model[:y])
 end
 
+function add_and_solve_model(land, cattle, sell, buy, yield, quota, plant, print_model = false, print_result = false)
+    model = add_model(land, cattle, sell, buy, yield, quota, plant, print_model)
+    solve_model(model, print_result)
+end
+
+
+
+
+function solve_multiple_models(land, cattle, sell_mean, sell_var, buy_mean, buy_var, yield_mean, yield_var, quota, plant, print_models = false, print_results = false)
+    sell_min = sell_mean * (1 - sell_var)
+    sell_max = sell_mean * (1 + sell_var)
+    buy_min = buy_mean * (1 - buy_var)
+    buy_max = buy_mean * (1 + buy_var)
+    yield_min = yield_mean * (1 - yield_var)
+    yield_max = yield_mean * (1 + yield_var)
+    add_and_solve_model(land, cattle, sell_min, buy_min, yield_min, quota, plant, print_models, print_results)
+    add_and_solve_model(land, cattle, sell_mean, buy_mean, yield_mean, quota, plant, print_models, print_results)
+    add_and_solve_model(land, cattle, sell_max, buy_max, yield_max, quota, plant, print_models, print_results)
+end
+
+#Parameters
+land = 500
+cattle = [200, 240]
+sell_mean = [170, 150, 36, 10]
+buy_mean = 1.4 * sell_mean[1:2]
+quota = 6000
+yield_mean = [2.5,3,20]
+plant = [150,230,260]
+
 sell_var = 0.0
 buy_var = 0.0
-yield_var = 0.0
-model1 = add_model(land, cattle, sell_mean, buy_mean, yield_mean, quota, plant)
+yield_var = 0.2
 
 
-objective, x, w, y = solve_model(model1)
-
-println("objective: ", objective)
-println(x,w,y)
-
-#(land, cattle, sell_mean, sell_var, buy_mean, buy_var, yield_mean, yield_var, quota, plant)
-
-#function solve_multiple_models(land, cattle, sell_mean, sell_var, buy_mean, buy_var, yield_mean, yield_var, quota, plant)
+solve_multiple_models(land, cattle, sell_mean, sell_var, buy_mean, buy_var, yield_mean, yield_var, quota, plant)
 
 
-
-println("end")
+println("END ______________________________________________________________________________________")
